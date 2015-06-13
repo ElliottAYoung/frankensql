2939289138ae5cf0e36fa86fc40a94c407305e52
Elliott Young <elliott.a.young@gmail.com>
2015-06-12 21:27:28 -0500

    Create base module

--- lib/frankensql.rb
+++ lib/frankensql.rb
@@ -1,5 +1,19 @@
 require "frankensql/version"
 
 module Frankensql
-  # Your code goes here...
+  def self.search(table, params)
+ 		query = ["SELECT * FROM #{table} WHERE id > 0"]
+ 		params.each_key{ |key| add_to_query(params.fetch(key), key) }
+ 		ActiveRecord::Base.connection.execute(query.join(" "))
+  end
+
+  private
+
+  def self.add_to_query(key, value)
+  	is_present?(param) ? (query << "AND #{key} LIKE '%#{value}%'") : nil
+  end
+
+  def self.is_present?(type)
+  	!type.empty? && !type.include?(";")
+  end
 end
